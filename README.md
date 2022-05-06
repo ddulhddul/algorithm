50c252bbda3f4f1f95f3f9babd340a2b64e1c2cd
ddulh <rjw88@naver.com>
2022-05-07 00:01:49 +0900

    1233 1915 init

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # [LeetCode](https://leetcode.com/)
+## medium
 - &check; [518. Coin Change 2](./src/leetCode/0518.js) - [Link](https://leetcode.com/problems/coin-change-2/)
 - &check; [1209. Remove All Adjacent Duplicates in String II](./src/leetCode/1209.js) - [Link](https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/)
+- [1233. Remove Sub-Folders from the Filesystem](./src/leetCode/1233.js) - [Link](https://leetcode.com/problems/remove-sub-folders-from-the-filesystem/)
+- [1915. Number of Wonderful Substrings](./src/leetCode/1915.js) - [Link](https://leetcode.com/problems/number-of-wonderful-substrings/)
---
+++ src/leetCode/1233.js
@@ -0,0 +1,20 @@
+/*
+1233. Remove Sub-Folders from the Filesystem
+https://leetcode.com/problems/remove-sub-folders-from-the-filesystem/
+
+Given a list of folders folder, return the folders after removing all sub-folders in those folders. You may return the answer in any order.
+
+If a folder[i] is located within another folder[j], it is called a sub-folder of it.
+
+The format of a path is one or more concatenated strings of the form: '/' followed by one or more lowercase English letters.
+
+For example, "/leetcode" and "/leetcode/problems" are valid paths while an empty string and "/" are not.
+
+*/
+/**
+ * @param {string[]} folder
+ * @return {string[]}
+ */
+const removeSubfolders = function (folder) {
+
+}
