84666dd0a87a01750aa1a766d0c0e1f6ffc61947
ddulh <ddulhddul@gmail.com>
2022-05-09 10:43:19 +0900

    init 1296

---
+++ src/leetCode/1296.js
@@ -0,0 +1,16 @@
+/*
+1296. Divide Array in Sets of K Consecutive Numbers
+https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/
+
+Given an array of integers nums and a positive integer k, check whether it is possible to divide this array into sets of k consecutive numbers.
+
+Return true if it is possible. Otherwise, return false.
+*/
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+const isPossibleDivide = function (nums, k) {
+
+}
