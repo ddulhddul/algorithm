f5c99acf799bdf88d751570d7368f30209f30c48
ddulh <ddulhddul@gmail.com>
2022-05-09 13:30:36 +0900

    ing 1296

--- src/leetCode/1296.js
+++ src/leetCode/1296.js
@@ -11,6 +11,46 @@ Return true if it is possible. Otherwise, return false.
  * @param {number} k
  * @return {boolean}
  */
+// eslint-disable-next-line no-unused-vars
 const isPossibleDivide = function (nums, k) {
+  if ((nums.length % k) !== 0) return false
+  if (k === 1) return true
 
+  const arr = new Array(1000000001)
+  for (let i = nums.length - 1; i >= 0; i--) {
+    arr[nums[i]] = (arr[nums[i]] || 0) + 1
+  }
+
+  const keyArr = arr.reduce((entry, obj, index) => {
+    entry.push(index)
+    return entry
+  }, [])
+
+  const len = keyArr.length
+  for (let i = 0; i < len; i++) {
+    const index = keyArr[i]
+    const value = arr[index]
+    if (value) {
+      if (value > 0) {
+        for (let i = k - 1; i >= 0; i--) {
+          arr[index + i] = (arr[index + i] || 0) - value
+        }
+      } else {
+        return false
+      }
+    }
+  }
+  return !arr.find((count) => count)
 }
+
+// isPossibleDivide([1, 2, 3, 4, 8, 3], 3)
+// console.log(isPossibleDivide([16, 21, 26, 35], 4))
+// console.log(isPossibleDivide([1,2,3,3,4,4,5,6], 4))
+// console.log(isPossibleDivide(testcase, 500))
+
+/*
+순회에서
+최소값부터 시작해서 k 개의 값들을 갯수로 제거하면되는데,
+전체 순회를 할 수 없는 부분 (time exeed)
+최소한 order 를 찾는 부분 다시 정의 필요
+*/
