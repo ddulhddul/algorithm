cd317c7ded9c612c6846f08ade95d28b7bfb7a30
ddulh <ddulhddul@gmail.com>
2022-05-12 14:44:20 +0900

    1915 ing

--- src/leetCode/1915.js
+++ src/leetCode/1915.js
@@ -15,7 +15,6 @@ A substring is a contiguous sequence of characters in a string.
  * @return {number}
  */
 const wonderfulSubstrings = function (word) {
-  // 'a'.charCodeAt(0) = 97
   let cur = 0
   let ans = 0
   const bitmapCnt = { 0: 1 }
@@ -43,4 +42,13 @@ https://www.krammerliu.com/blog/leetcode-1915-number-of-wonderful-substrings/
 : i부터 j까지 word의 wonderful string cnt
   = (0부터 j까지 cnt) - (0부터 i-1까지 cnt)
 - odd / even 만 구분하면 되므로 bit 연산자로 해결 ?
+## 비트연산자
+  - &	대응되는 비트가 모두 1이면 1을 반환함. (비트 AND 연산)
+  - |	대응되는 비트 중에서 하나라도 1이면 1을 반환함. (비트 OR 연산)
+  - ^	대응되는 비트가 서로 다르면 1을 반환함. (비트 XOR 연산)
+  - ~	비트를 1이면 0으로, 0이면 1로 반전시킴. (비트 NOT 연산)
+  - <<	지정한 수만큼 비트들을 전부 왼쪽으로 이동시킴. (left shift 연산)
+  - >>	부호를 유지하면서 지정한 수만큼 비트를 전부 오른쪽으로 이동시킴. (right shift 연산)
+
+https://stackoverflow.com/questions/69649911/count-number-of-wonderful-substrings
 */
