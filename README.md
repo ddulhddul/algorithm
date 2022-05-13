b760ef5436364b0cc06ea2c76d0116aa3a8ade87
ddulh <ddulhddul@gmail.com>
2022-05-13 13:41:58 +0900

    complete 1035

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 - &check; [518. Coin Change 2](./src/leetCode/0518.js) - [Link](https://leetcode.com/problems/coin-change-2/)
 - &check; [1209. Remove All Adjacent Duplicates in String II](./src/leetCode/1209.js) - [Link](https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/)
 - &check; [1233. Remove Sub-Folders from the Filesystem](./src/leetCode/1233.js) - [Link](https://leetcode.com/problems/remove-sub-folders-from-the-filesystem/)
-- [1035. Uncrossed Lines](./src/leetCode/1035.js) - [Link](https://leetcode.com/problems/uncrossed-lines/)
+- &check; [1035. Uncrossed Lines](./src/leetCode/1035.js) - [Link](https://leetcode.com/problems/uncrossed-lines/)
 - [1296. Divide Array in Sets of K Consecutive Numbers](./src/leetCode/1296.js) - [Link](https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/)
 - [1915. Number of Wonderful Substrings](./src/leetCode/1915.js) - [Link](https://leetcode.com/problems/number-of-wonderful-substrings/)
--- src/leetCode/1035.js
+++ src/leetCode/1035.js
@@ -19,5 +19,47 @@ Return the maximum number of connecting lines we can draw in this way.
  * @return {number}
  */
 const maxUncrossedLines = function (nums1, nums2) {
-
+  const len1 = nums1.length
+  const len2 = nums2.length
+  const arr = [[]]
+  let std = 0
+  // std 0 인 경우, 따로
+  for (let i = std; i < len2; i++) {
+    if (nums1[std] === nums2[i]) arr[std][i] = 1
+    else arr[std][i] = arr[std][i - 1] || 0
+  }
+  for (let i = std + 1; i < len1; i++) {
+    arr.push([])
+    if (nums2[std] === nums1[i]) arr[i][std] = 1
+    else arr[i][std] = (!arr[i - 1] ? 0 : arr[i - 1][std]) || 0
+  }
+  while (++std < len1) {
+    // console.log('std', std)
+    for (let i = std; i < len2; i++) {
+      if (nums1[std] === nums2[i]) {
+        arr[std][i] = arr[std - 1][i - 1] + 1
+      } else {
+        arr[std][i] = Math.max(arr[std - 1][i], arr[std][i - 1])
+      }
+    }
+    for (let i = std + 1; i < len1; i++) {
+      if (nums2[std] === nums1[i]) {
+        arr[i][std] = arr[i - 1][std - 1] + 1
+      } else {
+        arr[i][std] = Math.max(arr[i][std - 1], arr[i - 1][std])
+      }
+    }
+  }
+  return arr[len1 - 1][len2 - 1]
 }
+
+console.log(
+  // maxUncrossedLines([1, 4, 2], [1, 2, 4])
+  // maxUncrossedLines([2,5,1,2,5], [10,5,2,1,5,2])
+  maxUncrossedLines([1, 3, 7, 1, 7, 5], [1, 9, 2, 5, 1])
+  // maxUncrossedLines([3,2],[2,2,2,3])
+)
+
+/*
+i,j 일때, 직전의 ij 가 같은경우와 다른경우의 dp 를 memoization
+*/
