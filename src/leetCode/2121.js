ebaed0f052ba2b5b90c13fa1ba5832ac7735f0ae
ddulhddul <ddulhddul@gmail.com>
2022-05-28 22:14:25 +0900

    init 2121

---
+++ src/leetCode/2121.js
@@ -0,0 +1,19 @@
+/*
+2121. Intervals Between Identical Elements
+
+You are given a 0-indexed array of n integers arr.
+
+The interval between two elements in arr is defined as the absolute difference between their indices. More formally, the interval between arr[i] and arr[j] is |i - j|.
+
+Return an array intervals of length n where intervals[i] is the sum of intervals between arr[i] and each element in arr with the same value as arr[i].
+
+Note: |x| is the absolute value of x.
+
+*/
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+const getDistances = function (arr) {
+
+}
